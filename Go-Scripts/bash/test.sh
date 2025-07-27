6a57a8ea5ef4c9171c735dc16134f727c3024f79
rocket <adamrivard87@gmail.com>
2025-07-27 19:45:08 -0400

    s

--- Go-Scripts/bash/test.sh
+++ Go-Scripts/bash/test.sh
@@ -1,3 +1,30 @@
 #!/usr/bin/env bash
 
-echo "This is a test script."
+# Make sure gum is installed
+if ! command -v gum &> /dev/null; then
+  echo "Error: gum is not installed." >&2
+  exit 1
+fi
+
+# List of available Flatpak apps
+apps=(
+  "org.kde.marknote - KDE Marknote"
+  "io.github.zefr0x.hashes - Hashes"
+  "io.github.bytezz.IPLookup - IPLookup"
+)
+
+# Use gum to select which apps to install
+selected=$(printf "%s\n" "${apps[@]}" | gum choose --no-limit --header="Select Flatpak apps to install:")
+
+# Exit if nothing was selected
+if [ -z "$selected" ]; then
+  echo "No apps selected. Exiting."
+  exit 0
+fi
+
+# Loop through selected apps and install each
+while read -r line; do
+  app_id=$(echo "$line" | awk '{print $1}')
+  echo "Installing $app_id..."
+  flatpak install -y flathub "$app_id"
+done <<< "$selected"
