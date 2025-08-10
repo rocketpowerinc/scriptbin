39b9f28931f6d2d531ac811f61bb2e79a543ecf8
rocket <adamrivard87@gmail.com>
2025-08-10 11:59:45 -0400

    e

--- Boiler Plate/Linux_Boiler_plate.sh
+++ Boiler Plate/Linux_Boiler_plate.sh
@@ -1,15 +1,38 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-#*Tags
-#bash #pwsh #zsh #fish
-#Linux #Mac #Server #WSL
-#Ubuntu #Debian #Fedora #Arch #NixOS #RaspberryPi
-#ARM64/AArch64 #x86_64
-#Wayland #x11 
-#apt #dnf #pacman #nix #brew
-#Gnome #kde #hyperland #xfce
 
+#*Tags:
+# Languages: bash pwsh zsh fish
+# Platforms: Linux Mac Server WSL
+# Distros: Ubuntu Debian Fedora Arch NixOS RaspberryPi
+# Architectures: ARM64/AArch64 x86_64
+# DisplayServers: Wayland x11
+# PackageManagers: apt dnf pacman nix brew
+# DesktopEnvironments: Gnome kde hyprland xfce
+# Categories: utility development git
+
+
+
+############* Temp CLone Repository Snippet ############
+# Config
+REPO_URL="https://github.com/rocketpowerinc/xxx.git"
+DOWNLOAD_PATH="$HOME/Downloads/Temp/xxx"
+
+# Make sure parent directory exists
+mkdir -p "$(dirname "$DOWNLOAD_PATH")"
+
+# Remove old copy if it exists
+if [ -d "$DOWNLOAD_PATH" ]; then
+    echo "Removing old folder: $DOWNLOAD_PATH"
+    rm -rf "$DOWNLOAD_PATH"
+fi
+
+# Clone repository
+echo "Cloning $REPO_URL into $DOWNLOAD_PATH..."
+git clone "$REPO_URL" "$DOWNLOAD_PATH"
+
+echo "Done!"
 
 
 
