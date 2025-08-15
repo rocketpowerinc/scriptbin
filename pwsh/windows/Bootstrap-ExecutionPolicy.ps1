49af1f12e398085e0f48dc348e299bedb9e1e4c6
ReevesA1 <adamrivard87@gmail.com>
2025-08-15 19:39:39 -0400

    s

--- pwsh/windows/Bootstrap-ExecutionPolicy.ps1
+++ pwsh/windows/Bootstrap-ExecutionPolicy.ps1
@@ -8,25 +8,25 @@
 # Privileges: admin
 #*############################################
 
+# If not running with Bypass, restart script with Bypass
+if ($ExecutionContext.SessionState.LanguageMode -ne 'FullLanguage') {
+  Write-Host "Restarting with ExecutionPolicy Bypass..."
+  powershell -NoProfile -ExecutionPolicy Bypass -File "`"$PSCommandPath`""
+  exit
+}
+
 Write-Host "Setting execution policy to RemoteSigned for the current user ... Press Enter to continue" -ForegroundColor Green
 Read-Host
 
-# Ensure the security module is loaded
 Import-Module Microsoft.PowerShell.Security -ErrorAction Stop
 
-try {
-  $ExecutionPolicy = Get-ExecutionPolicy -Scope CurrentUser
-  if ($ExecutionPolicy -eq "RemoteSigned") {
-    Write-Verbose "Execution policy is already set to RemoteSigned for the current user, skipping..." -Verbose
-  }
-  else {
-    Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy RemoteSigned -Force
-    Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass -Force
-    Write-Host "Execution policy updated." -ForegroundColor Green
-  }
+$ExecutionPolicy = Get-ExecutionPolicy -Scope CurrentUser
+if ($ExecutionPolicy -eq "RemoteSigned") {
+  Write-Verbose "Execution policy is already set to RemoteSigned for the current user, skipping..." -Verbose
 }
-catch {
-  Write-Host "Error: $($_.Exception.Message)" -ForegroundColor Red
+else {
+  Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy RemoteSigned -Force
+  Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass -Force
 }
 
 Write-Host "`nFinal Execution Policy settings:" -ForegroundColor Cyan
