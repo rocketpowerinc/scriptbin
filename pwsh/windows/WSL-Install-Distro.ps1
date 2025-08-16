56062c987ab8df9a458ef09a20c1b886a50bfc8d
ReevesA1 <adamrivard87@gmail.com>
2025-08-15 20:19:15 -0400

    s

--- pwsh/windows/WSL-Install-Distro.ps1
+++ pwsh/windows/WSL-Install-Distro.ps1
@@ -10,10 +10,11 @@
 # Privileges: admin
 
 #*############################################
-
-# Get the list of distro names from WSL
-$distros = wsl.exe --list --online | Select-Object -Skip 1 | ForEach-Object {
-    ($_ -split '\s+')[0]
+# Get the list of distro names from WSL (skip header and empty lines)
+$distros = wsl.exe --list --online | ForEach-Object {
+  $_.Trim()
+} | Where-Object { $_ -and ($_ -notmatch '^NAME') } | ForEach-Object {
+  ($_ -split '\s+')[0]
 }
 
 # Convert to newline-separated string
@@ -22,10 +23,11 @@ $distrosText = $distros -join "`n"
 # Use Gum to select multiple distros
 $selected = Write-Output $distrosText | gum choose --no-limit
 
-# Install each selected distro using -d flag
-$selected.Split("`n") | ForEach-Object {
-    if ($_ -ne "") {
-        Write-Host "Installing $_..."
-        wsl.exe --install -d $_
-    }
+# Install each selected distro
+$selected -split "`r?`n" | ForEach-Object {
+  $distro = $_.Trim()
+  if ($distro) {
+    Write-Host "Installing $distro..."
+    wsl.exe --install -d $distro
+  }
 }
