86c28f5d807586d224b011422933833d3d877199
ReevesA1 <adamrivard87@gmail.com>
2026-01-24 08:21:26 -0500

    s

---
+++ Third-party/LinuxToys-GUI.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+#*Tags:
+# Name: CTT-WinUtil.ps1
+# Shell: pwsh
+# Platforms: Windows
+# Hardware: ROG-Ally ROG-Desktop ROG-Laptop ThinkPad
+# Version: Windows10 Windows11
+# Architectures: ARM64/AArch64 x86_64
+# Framework: Gum
+# PackageManagers: winget choco
+# Type: Bootstrap Appbundle Utility
+# Categories: development customization
+# Privileges: admin
+# ThirdParty: Titus
+
+
+#*############################################
+
+curl -fsSL https://linux.toys/install.sh | bash
