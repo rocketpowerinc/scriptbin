8f7efea948f5e046fdc287f69d2d698aa5aa257d
ReevesA1 <adamrivard87@gmail.com>
2026-02-10 05:58:30 -0500

    s

---
+++ Third-party/ChristianLempa-BoilerPlates.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+#*Tags:
+# Name: ChristianLempa-BoilerPlates.sh
+# Shell: bash
+# Platforms: Linux-GUI Server
+# Hardware: RaspberryPi Steamdeck ROG-Ally ROG-Desktop ROG-Laptop ThinkPad Android Docker
+# Distros: Ubuntu Debian Fedora Arch NixOS Opensuse Atomic
+# Architectures: ARM64/AArch64 x86_64
+# DisplayServers: Wayland x11
+# PackageManagers: apt dnf pacman flatpak
+# DesktopEnvironments: Gnome kde hyprland xfce
+# Type: Bootstrap Appbundle Utility
+# Categories: development customization docker
+# Privileges: admin
+# ThirdParty: ChristianLempa
+
+
+#*############################################
+
+curl -fsSL https://raw.githubusercontent.com/christianlempa/boilerplates/main/scripts/install.sh | bash
