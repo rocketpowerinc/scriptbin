1c1607d1afe0871b002d1e0cce566b578d666cc1
ReevesA1 <adamrivard87@gmail.com>
2026-02-21 12:56:58 -0500

    d

--- activate-go-pwr/AnduinOS.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-sudo apt update && sudo apt upgrade
-sudo apt install golang-go
-echo 'export PATH="$HOME/go/bin:$PATH"' >> ~/.bashrc
-sudo apt update && sudo apt install -y git gh jq make bat tmux curl wget glow gum
-go install -v github.com/rocketpowerinc/go-pwr/cmd/go-pwr@latest
-source ~/.bashrc
-go-pwr
